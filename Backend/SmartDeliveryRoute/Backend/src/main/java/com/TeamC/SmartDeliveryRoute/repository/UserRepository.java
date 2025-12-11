da8c1d45b99074ae78d995fc855a49d5ccf0cc9e
tejast22 <tejasthakor2001@gmail.com>
2025-12-11 17:27:35 +0530

    final changes in the backend files uploded

--- Backend/SmartDeliveryRoute/Backend/src/main/java/com/TeamC/SmartDeliveryRoute/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.TeamC.SmartDeliveryRoute.repository;
-
-import com.TeamC.SmartDeliveryRoute.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String>{
-    boolean existsByEmail(String email);
-}
